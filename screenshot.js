071dc8a8e7313c10088f062b440fa7e3d16ec82b
naresh335 <nareshbansal225@gmail.com>
2024-09-05 09:45:29 +0530

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -3,6 +3,10 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,  // Set the width to 1920px
+    height: 1080, // Set the height to 1080px
+  });
   await page.goto('https://coderootz.com');
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
